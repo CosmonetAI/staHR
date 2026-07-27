92d927e7dcfbd6a315b0f4e166a000ce2213ee38
soumyashree <soumyashreepatil.snip@gmail.com>
2026-07-27 14:38:08 +0530

    deleted staHR folder

--- staHR/src/data/sampleUsers.ts
+++
@@ -1,5 +0,0 @@
-export const sampleUsers = [
-  { id: '1', email: 'admin@peopleos.test', password: 'admin123', name: 'Super Admin', role: 'super_admin' },
-  { id: '2', email: 'hr@peopleos.test', password: 'hr12345', name: 'HR Admin', role: 'hr_admin' },
-  { id: '3', email: 'recruiter@peopleos.test', password: 'rec12345', name: 'Recruiter', role: 'recruiter' }
-]
